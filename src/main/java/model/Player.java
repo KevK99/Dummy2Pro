36a7ef4d201783f04a90ef864c0be7ca2b521728
Kevin Klebula <k.klebula@diskover.de>
2026-02-06 09:29:56 +0100

    add file structure

---
+++ src/main/java/model/Player.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Player
+{
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private int score;
+
+    public Player(String name)
+    {
+        this.name = name;
+        this.score = 0;
+    }
+
+    public void addScore(int points)
+    {
+        this.score += points;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getScore()
+    {
+        return score;
+    }
+}
