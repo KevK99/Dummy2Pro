0ed82aa56c3e02c661e8447bcf0643fa695c74dd
Kevin Klebula <k.klebula@diskover.de>
2026-02-24 07:25:46 +0100

    correct project structure

---
+++ src/main/java/me/daskabel/dummy2pro/Dummy2proApplication.java
@@ -0,0 +1,13 @@
+package me.daskabel.dummy2pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dummy2proApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dummy2proApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/daskabel/dummy2pro/Dummy2proApplicationTests.java
@@ -0,0 +1,13 @@
+package me.daskabel.dummy2pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dummy2proApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
